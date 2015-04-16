a7c1da310e77fbcef5db8566eb9a363bec5fbbd2
Wes <wes.stclair@gmail.com>
2015-04-16 12:39:15 -0500

    first failing test

--- cms/tests.py
+++ cms/tests.py
@@ -1,6 +1,17 @@
 from django import test
 
+from cms.models import Navigation, Article
+
+
 class CmsTests(test.TestCase):
 
-    def test_whatevs(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        article = Article.objects.create(slug="test", text="We are here!")
+        Navigation.objects.create(article=article, text="test", order=1)
+
+    def test_second_request_should_not_touch_database(self):
+        response = self.client.get("/")
+        self.assertTrue("We are here!" in response.content)
+        with self.assertNumQueries(0):
+            response = self.client.get("/")
+            self.assertTrue("We are here!" in response.content)
